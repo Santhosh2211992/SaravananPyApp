0be6e3991895b04a707807116dbb43fd4c7814e0
Saravanan <Saravanan.Narayanan@test.com>
2018-09-23 21:12:52 +0530

    Modified builder

--- Builder.py
+++ Builder.py
@@ -5,6 +5,8 @@ import datetime
 
 OUTPUT = "output_" + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ".txt"
 
+print("[INFO] : Builder.py started executing")
+
 cwd = os.getcwd()
 repoPath = sys.argv[1]
 os.chdir(repoPath)
@@ -17,15 +19,24 @@ if not os.path.exists('bin'):
 outputfile = open('bin' + '\\' + OUTPUT, 'w+')
 
 for line in inputfile1:
+    print("[INFO] : Processing data from file i.txt")
 	outputfile.write(line)
 for line in inputfile2:
+    print("[INFO] : Processing data from file am.txt")
 	outputfile.write(line)
 for line in inputfile3:
+    print("[INFO] : Processing data from file myName.txt")
+	print("[INFO] : Preparing the file " + OUTPUT)
 	outputfile.write(line)
 
+	
 inputfile1.close
 inputfile2.close
 inputfile3.close
 outputfile.close
 
 os.chdir(cwd)
+
+print("**********************************************************")
+print("build.bat completed successfully")
+print("**********************************************************")
